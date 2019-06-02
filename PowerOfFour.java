35919ef51475a251b6757e68e8cb9ecd107d2eb1
Madhuri Chadalapaka <madhuri.chadalapaka@gmail.com>
2019-06-02 13:09:28 -0700

    Update PowerOfFour.java

--- PowerOfFour.java
+++ PowerOfFour.java
@@ -1,7 +1,7 @@
 public class PowerOfFour {
 static boolean isPowerOfFour(int n) { 
     // Your Logic Goes Here...
-   // Return true when the number is a power of 2 and the remainder is 1 		when divided by 3
+   // Return true when the number is a power of 2 and the remainder is 1 when divided by 3
   	return (((n&(n-1)) == 0) && (n % 3 == 1));
 }
 public static void main(String args[]) {
